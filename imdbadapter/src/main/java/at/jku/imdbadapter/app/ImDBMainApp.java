f79ff606a28f645b9ea263dba043e220ca5b263f
Markus <Markus.Weissenbek@gmx.at>
2016-03-06 23:59:05 +0100

    initCommit_

--- imdbadapter/src/main/java/at/jku/imdbadapter/app/ImDBMainApp.java
+++ imdbadapter/src/main/java/at/jku/imdbadapter/app/ImDBMainApp.java
@@ -1,7 +1,5 @@
 package at.jku.imdbadapter.app;
 
-import java.util.List;
-
 import at.jku.imdbadapter.model.Movie;
 import at.jku.imdbadapter.rest.ImDBRESTServiceClient;
 
@@ -9,14 +7,14 @@ public class ImDBMainApp {
 
 	public static void main(String[] args) {
 		ImDBRESTServiceClient client = new ImDBRESTServiceClient();
-		// Movie movie = client.getMovieByTitle("Game");
-		// System.out.println(movie.getActors());
+		Movie movie = client.getMovieByTitle("Game");
+		System.out.println(movie.getActors());
 
 		// movie = client.getMovieByID("tt0944947");
 		// System.out.println(movie.getAwards());
 
-		List<Movie> movieList = client.getMoviesBySearch("Game");
-		System.out.println(movieList.get(0).getAwards());
+		// List<Movie> movieList = client.getMoviesBySearch("Game");
+		// System.out.println(movieList.get(0).getAwards());
 
 	}
 }
