ce072755764a26b61b1c3ec4bc389fa46e2579f0
Berthold <berthold.roiser@gmail.com>
2016-03-09 16:07:44 +0100

    javadoc

--- imdbadapter/src/main/java/at/jku/imdbadapter/rest/ImbdSearchAPI.java
+++ imdbadapter/src/main/java/at/jku/imdbadapter/rest/ImbdSearchAPI.java
@@ -8,21 +8,43 @@ import at.jku.imdbadapter.model.ombd.Movie;
 import at.jku.imdbadapter.task.MovieTask;
 import at.jku.imdbadapter.task.SearchTask;
 
+/**
+ * 
+ * This search API enables you to look for detail on movies, series and games
+ * from {@link http://http://www.imdb.com/}.
+ * 
+ * @author Berthold
+ *
+ */
 public final class ImbdSearchAPI {
+
     public enum Type {
         MOVIE, SERIES, GAME
     }
 
     private final ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
 
+    /**
+     * Find movie by id
+     *
+     * @param id
+     * @return movie
+     */
     public Movie findByImdbId(String id) {
         String movieURL = new ImdbUrlBuilder().setId(id).build();
         return forkJoinPool.invoke(new MovieTask(movieURL));
     }
 
-    public List<Movie> search(String searchString, Type type) {
+    /**
+     * Search for movies by specific type and title
+     * 
+     * @param title
+     * @param type
+     * @return list of movies
+     */
+    public List<Movie> search(String title, Type type) {
         String typeAsString = type.name().toLowerCase();
-        String searchURL = new ImdbUrlBuilder().setSearchTitle(searchString).setType(typeAsString).build();
+        String searchURL = new ImdbUrlBuilder().setSearchTitle(title).setType(typeAsString).build();
         return forkJoinPool.invoke(new SearchTask(searchURL, true));
     }
 }
